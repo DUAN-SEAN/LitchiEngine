2fe1deaf188f13144a4be87345dd9228a7a94d08
SeanDuan <404064259@qq.com>
2024-03-07 13:05:15 +0800

    [Engine] [Scene] refactor sceneManager

--- Engine/Source/Runtime/Function/Framework/Component/UI/UIText.cpp
+++ Engine/Source/Runtime/Function/Framework/Component/UI/UIText.cpp
@@ -15,8 +15,7 @@
 using namespace rttr;
 using namespace LitchiRuntime;
 
-UIText::UIText():m_color({1,1,1,1}) {
-
+UIText::UIText() :m_color({ 1,1,1,1 }), m_dirty{ true } {
 }
 
 void UIText::SetText(std::string text) {
@@ -27,44 +26,19 @@ void UIText::SetText(std::string text) {
     m_dirty=true;
 }
 
-void LitchiRuntime::UIText::OnAwake()
+void UIText::OnAwake()
 {
 }
 
 void UIText::OnUpdate() {
     Component::OnUpdate();
 
+    UpdateTextContent();
+}
 
-    if(m_font == nullptr)
-    {
-        return;
-    }
-
-    if(m_dirty)
-    {
-        TextData text_data = m_font->GetTextData(m_text);
-
-        // create/grow buffers
-    	if (text_data.vertices.size() != m_vertex_buffer->GetVertexCount())
-        {
-            m_vertex_buffer->CreateDynamic<RHI_Vertex_PosTex>(static_cast<uint32_t>(text_data.vertices.size()));
-            m_index_buffer->CreateDynamic<uint32_t>(static_cast<uint32_t>(text_data.indices.size()));
-        }
-
-        // copy the data over to the gpu
-        {
-            if (RHI_Vertex_PosTex* vertex_buffer = static_cast<RHI_Vertex_PosTex*>(m_vertex_buffer->GetMappedData()))
-            {
-	            std::copy(text_data.vertices.begin(), text_data.vertices.end(), vertex_buffer);
-            }
-
-            if (uint32_t* index_buffer = static_cast<uint32_t*>(m_index_buffer->GetMappedData()))
-            {
-	            std::copy(text_data.indices.begin(), text_data.indices.end(), index_buffer);
-            }
-        }
-
-    }
+void UIText::OnEditorUpdate()
+{
+    UpdateTextContent();
 }
 
 void UIText::OnPreRender() {
@@ -80,6 +54,7 @@ void UIText::PostResourceLoaded()
     CreateTextBuffer();
     // default
     PostResourceModify();
+    UpdateTextContent();
 }
 
 void UIText::PostResourceModify()
@@ -97,6 +72,40 @@ void UIText::CreateTextBuffer()
         // create buffer
         m_vertex_buffer = std::make_shared<RHI_VertexBuffer>(true, "font");
         m_index_buffer = std::make_shared<RHI_IndexBuffer>(true, "font");
+    }
+}
+
+void UIText::UpdateTextContent()
+{
+    if (m_font == nullptr)
+    {
+        return;
+    }
+
+    if (m_dirty)
+    {
+        TextData text_data = m_font->GetTextData(m_text);
+
+        // create/grow buffers
+        if (text_data.vertices.size() != m_vertex_buffer->GetVertexCount())
+        {
+            m_vertex_buffer->CreateDynamic<RHI_Vertex_PosTex>(static_cast<uint32_t>(text_data.vertices.size()));
+            m_index_buffer->CreateDynamic<uint32_t>(static_cast<uint32_t>(text_data.indices.size()));
+        }
+
+        // copy the data over to the gpu
+        {
+            if (RHI_Vertex_PosTex* vertex_buffer = static_cast<RHI_Vertex_PosTex*>(m_vertex_buffer->GetMappedData()))
+            {
+                std::copy(text_data.vertices.begin(), text_data.vertices.end(), vertex_buffer);
+            }
+
+            if (uint32_t* index_buffer = static_cast<uint32_t*>(m_index_buffer->GetMappedData()))
+            {
+                std::copy(text_data.indices.begin(), text_data.indices.end(), index_buffer);
+            }
+        }
+
     }
 }
 
