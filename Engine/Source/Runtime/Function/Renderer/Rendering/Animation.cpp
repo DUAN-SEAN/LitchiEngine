ebbbe7251264bab7ea2d5552af1b83e69af48dc1
SeanDuan <404064259@qq.com>
2024-08-26 23:32:53 +0800

    [Engine] [Animation] upgrade Animation Lerp Perf

--- Engine/Source/Runtime/Function/Renderer/Rendering/Animation.cpp
+++ Engine/Source/Runtime/Function/Renderer/Rendering/Animation.cpp
@@ -11,6 +11,15 @@ using namespace std;
 
 namespace LitchiRuntime
 {
+
+	bool compareByTimePos1(const VectorKey& a, const VectorKey& b) {
+		return a.timePos < b.timePos;
+	}
+
+	bool compareByTimePos2(const QuatKey& a, const QuatKey& b) {
+		return a.timePos < b.timePos;
+	}
+
 	float BoneAnimation::GetStartTime()const {
 		float t0 = 0.0f;
 		float t1 = 0.0f;
@@ -23,6 +32,8 @@ namespace LitchiRuntime
 		timePos = timePos < t2 ? timePos : t2;
 		return timePos;
 	}
+
+
 
 	float BoneAnimation::GetEndTime()const {
 		float t0 = 0.0f;
@@ -39,16 +50,30 @@ namespace LitchiRuntime
 
 	LitchiRuntime::Vector3 BoneAnimation::LerpKeys(float t, const std::vector<VectorKey>& keys) {
 		Vector3 res = Vector3(0.0f);
+
+
+
 		if (t <= keys.front().timePos) res = keys.front().value;
 		else if (t >= keys.back().timePos) res = keys.back().value;
 		else {
-			for (size_t i = 0; i < keys.size() - 1; i++) {
+
+			auto finditr = std::lower_bound(keys.begin(), keys.end(), VectorKey{t,Vector3::Zero}, compareByTimePos1);
+
+			if(finditr != keys.end() && std::next(finditr) != keys.end())
+			{
+				auto first = *finditr;
+				auto second = *std::next(finditr);
+				float lerpPercent = (t - first.timePos) / (second.timePos - first.timePos);
+				res = first.value * lerpPercent + second.value * (1 - lerpPercent);
+			}
+
+		/*	for (size_t i = 0; i < keys.size() - 1; i++) {
 				if (t >= keys[i].timePos && t <= keys[i + 1].timePos) {
 					float lerpPercent = (t - keys[i].timePos) / (keys[i + 1].timePos - keys[i].timePos);
 					res = keys[i].value * lerpPercent + keys[i + 1].value * (1 - lerpPercent);
 					break;
 				}
-			}
+			}*/
 		}
 		return res;
 	}
@@ -58,13 +83,24 @@ namespace LitchiRuntime
 		if (t <= keys.front().timePos) res = keys.front().value;
 		else if (t >= keys.back().timePos) res = keys.back().value;
 		else {
-			for (size_t i = 0; i < keys.size() - 1; i++) {
+
+			auto finditr = std::lower_bound(keys.begin(), keys.end(), QuatKey{ t,Quaternion::Identity }, compareByTimePos2);
+
+			if (finditr != keys.end() && std::next(finditr) != keys.end())
+			{
+				auto first = *finditr;
+				auto second = *std::next(finditr);
+				float lerpPercent = (t - first.timePos) / (second.timePos - first.timePos);
+				res = Quaternion::Lerp(first.value, second.value, lerpPercent);
+			}
+
+			/*for (size_t i = 0; i < keys.size() - 1; i++) {
 				if (t >= keys[i].timePos && t <= keys[i + 1].timePos) {
 					float lerpPercent = (t - keys[i].timePos) / (keys[i + 1].timePos - keys[i].timePos);
 					res = Quaternion::Lerp(keys[i].value, keys[i + 1].value, lerpPercent);
 					break;
 				}
-			}
+			}*/
 		}
 		return res;
 	}
